055ac71e49fdc95200715395d8400ca3550ae50f
محمدمهدی بلوکیان <mohammadmahdiblokian@gmail.com>
2025-02-22 10:56:18 +0330

    Update Controls.tsx
    بفرما داش

--- components/main/Controls.tsx
+++ components/main/Controls.tsx
@@ -30,6 +30,7 @@ export const Controls = ({ deviceNumber }: ControlsProps) => {
       lock: "قفل کردن درب‌ها",
       bell: "فعال کردن آژیر",
       silent: "غیرفعال کردن آژیر",
+      power_off: "خاموش شدن",
     };
 
     const message = messages[action];
@@ -43,6 +44,7 @@ export const Controls = ({ deviceNumber }: ControlsProps) => {
     { id: "lock", icon: Lock, tooltip: "قفل کردن" },
     { id: "bell", icon: Bell, tooltip: "زنگ" },
     { id: "silent", icon: BellOff, tooltip: "سکوت" },
+    { id: "power_off", icon: power_off, tooltip: "خاموش" },
   ];
 
   return (
